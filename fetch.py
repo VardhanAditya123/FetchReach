8abd290bfe4d295cf7433c4661c184688bcd01e7
Aditya Vardhan <vardhan@adityas-air.attlocal.net>
2022-11-06 20:34:19 -0500

    DDPG + HER + FetchReach + mth

--- fetch.py
+++ fetch.py
@@ -29,11 +29,11 @@ aoptimizer = Adam(anetwork.parameters(), lr=pi_lr)
 qoptimizer = Adam(qnetwork.parameters(), lr=q_lr)
 
 def get_action(o, noise_scale):
-   
     act_limit = env.action_space.high[0]
     act_dim = env.action_space.shape[0]
     a = anetwork(torch.as_tensor(o, dtype=torch.float32)).detach().numpy()
-    a += noise_scale * np.random.randn(act_dim)
+    # a += noise_scale * np.random.randn(act_dim)
+    a = (1-noise_scale)*a  + noise_scale * np.random.randn(act_dim)
     return np.clip(a, -act_limit, act_limit)
 
 def compute_loss_q(data):
@@ -125,9 +125,10 @@ def main():
             
             if(steps  % 1 == 0 and episode > 200):
                update(replay_buffer)
-            
-    while(1):
-        test_agent()
+    
+    torch.save(anetwork, 'anetwork.pth')        
+    # while(1):
+    #     test_agent()
             
 
 def update(replay_buffer):
@@ -164,25 +165,22 @@ def update(replay_buffer):
 
 
 def test_agent():
-        num_test_episodes=10
-        avg_score_test = []
-        max_ep_len = 100000
-        for j in range(num_test_episodes):
-            o, d, ep_ret, ep_len = env.reset(), False, 0, 0
-            g = o['desired_goal']
-            o = o['observation']
-            while not (d or (ep_len == max_ep_len)):
-                # Take deterministic actions at test time (noise_2scale=0)
-                env.render()
-                newo = np.append(o,g)
-                action = get_action(newo, 0)
-                o, r, d, info = env.step(action)
-                print(r,info,action)
-                o = o['observation']
-                ep_ret += r
-                ep_len += 1
-            avg_score_test.append(ep_ret)
-        print(ep_ret)
+    avg_score_test = []
+    o, d, ep_ret, ep_len = env.reset(), False, 0, 0
+    g = o['desired_goal']
+    o = o['observation']
+    while not (d):
+        # Take deterministic actions at test time (noise_2scale=0)
+        env.render()
+        newo = np.append(o,g)
+        action = get_action(newo, 0)
+        o, r, d, info = env.step(action)
+        print(r,info,action)
+        o = o['observation']
+        ep_ret += r
+        ep_len += 1
+    avg_score_test.append(ep_ret)
+    print(ep_ret)
    
 if __name__ == '__main__':
     main()
